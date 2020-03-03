ea1a40205ad00a7e20af34ce1d8f97dc5497540f
ankurpatel1 <ankur.patel.0101@gmail.com>
2020-03-03 11:51:26 +0530

    first

---
+++ Utility/MonoUtility/FPSCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MonoUtility
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        private Text mText;
+
+        private void Awake()
+        {
+            mText = GetComponent<Text>();
+        }
+        void Update()
+        {
+            if (mText != null)
+                mText.text = "FPS: " + (int)(1 / Time.deltaTime);
+        }
+    }
+}
