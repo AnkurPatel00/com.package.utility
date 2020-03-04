f92a3c9cd43e22d554109d85e718b6d58ae62067
ankurpatel1 <ankur.patel.0101@gmail.com>
2020-03-04 12:06:02 +0530

    Updated ExtensionUtil

--- Runtime/ExtensionUtil.cs
+++ Runtime/ExtensionUtil.cs
@@ -12,6 +12,14 @@ namespace Utility
             return list[rng.Next(list.Count)];
         }
 
+        public static T GetLast<T>(this IList<T> list)
+        {
+            return list[list.Count - 1];
+        }
+        public static void RemoveFromLast<T>(this IList<T> list)
+        {
+            list.RemoveAt(list.Count - 1);
+        }
         public static T RandomElement<T>(this T[] array)
         {
             return array[rng.Next(array.Length)];
