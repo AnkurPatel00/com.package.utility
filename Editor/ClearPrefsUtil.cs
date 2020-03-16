5042271a5ec8931fa866607f0d01693ef978691b
ankurpatel1 <ankur.patel.0101@gmail.com>
2020-03-16 15:17:32 +0530

    Added ClearPrefsUtil

---
+++ Editor/ClearPrefsUtil.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MonoUtility
+{
+    public class ClearPrefsUtil : EditorWindow
+    {
+        [MenuItem("Tools/ClearAllPrefs")]
+        public static void ClearAllPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("All PlayerPrefs were deleted");
+        }
+    }
+}
